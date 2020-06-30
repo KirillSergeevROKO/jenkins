4b93c7c5e901034598a311444990483fea5bdcf4
Kirill Sergeev <kirill.sergeev@rokolabs.com>
2020-06-30 23:07:14 +0400

    add IFP staging mssql

--- ifp/STAGING/mssql/RunIFP_mssql.py
+++ ifp/STAGING/mssql/RunIFP_mssql.py
@@ -12,7 +12,7 @@ print('Running RunIFP.py...')
 #Read in configuration file
 cwd = os.getcwd()
 extractFileLocation = r'\\STAGING-ROKO-APPSERVER\InformentFiles\DATA'
-achdate = datetime.today() #- timedelta(days=1)
+achdate = datetime.today() - timedelta(days=1)
 
 with open(os.path.join(cwd, 'ifp\\STAGING\\\mssql\FileCheck_config.json')) as f:
     config = json.load(f)
