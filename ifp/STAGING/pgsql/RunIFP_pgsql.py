92ddbad5e95c3cdce0dd042a203b0db686d8000a
Kirill Sergeev <kirill.sergeev@rokolabs.com>
2020-07-01 15:12:55 +0400

    PGSQl staging

--- ifp/STAGING/pgsql/RunIFP_pgsql.py
+++ ifp/STAGING/pgsql/RunIFP_pgsql.py
@@ -21,7 +21,7 @@ with open(os.path.join(cwd, 'ifp\\STAGING\\\pgsql\FileCheck_config.json')) as f:
     importDate = achdate.strftime("%Y-%m-%d")
     payload = {
         'IsManual': 'false',
-        'ImportDate': importDate,
+        'ImportDate': '2020-06-25',
         'CanImportTransactionTables': 'true',
         'CanImportHistoryTables': 'true',
         'CanTruncateTransactionTables': 'true',
