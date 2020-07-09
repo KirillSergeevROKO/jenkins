482d5ceb636cbed1261e422e1f99f8ec69dfaa05
Kirill Sergeev <kirill.sergeev@rokolabs.com>
2020-07-09 16:40:21 +0400

    add big part of nightroutine

--- ifp/STAGING/mssql/RunIFP_mssql.py
+++ ifp/STAGING/mssql/RunIFP_mssql.py
@@ -21,7 +21,7 @@ with open(os.path.join(cwd, 'ifp\\STAGING\\\mssql\FileCheck_config.json')) as f:
     importDate = achdate.strftime("%Y-%m-%d")
     payload = {
         'IsManual': 'false',
-        'ImportDate': importDate,
+        'ImportDate': '2020-06-29',
         'CanImportTransactionTables': 'true',
         'CanImportHistoryTables': 'true',
         'CanTruncateTransactionTables': 'true',
