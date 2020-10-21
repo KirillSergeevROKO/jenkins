a211ec20536f7354fce7ddab0c9c123c80394b00
ddcroit <diegoCroitoru@gmail.com>
2020-10-21 13:33:16 -0300

    fix : in if

--- ifp/DEV/pgsql/RunIFP_pgsql.py
+++ ifp/DEV/pgsql/RunIFP_pgsql.py
@@ -21,10 +21,8 @@ with open(os.path.join(cwd, 'ifp\\DEV\\\pgsql\FileCheck_config.json')) as f:
     #call API
     API_ENDPOINT = config['ServicingApi']
     OverwriteImportDate =  os.getenv("OverwriteImportDate")
-
-# or OverwriteImportDate is None:
-
-    if OverwriteImportDate == "1900-01-01" 
+    
+    if OverwriteImportDate == "1900-01-01": # or OverwriteImportDate is None:
         importDate = achdate.strftime(DateFormat)
     else:
         importDate = datetime.strptime(OverwriteImportDate, DateFormat).strftime(DateFormat)
