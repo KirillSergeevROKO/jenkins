93c7635e7b62d455d41ae227e879fc5c04a13f36
ddcroit <diegoCroitoru@gmail.com>
2020-10-21 14:34:19 -0300

    no params no variable

--- ifp/DEV/pgsql/RunIFP_pgsql.py
+++ ifp/DEV/pgsql/RunIFP_pgsql.py
@@ -16,16 +16,18 @@ DateFormat = "%Y-%m-%d"
 achdate = datetime.today() # - timedelta(days=1)
 IsManual = 'false'
 
+importDate = achdate.strftime(DateFormat)
+
 with open(os.path.join(cwd, 'ifp\\DEV\\\pgsql\FileCheck_config.json')) as f:
     config = json.load(f)
     #call API
     API_ENDPOINT = config['ServicingApi']
-    OverwriteImportDate =  os.getenv("OverwriteImportDate")
+    #OverwriteImportDate =  os.getenv("OverwriteImportDate")
     
-    if OverwriteImportDate == "1900-01-01" or OverwriteImportDate is None:
-        importDate = achdate.strftime(DateFormat)
-    else:
-        importDate = datetime.strptime(OverwriteImportDate, DateFormat).strftime(DateFormat)
+    #if OverwriteImportDate == "1900-01-01" or OverwriteImportDate is None:
+    #    importDate = achdate.strftime(DateFormat)
+    #else:
+    #    importDate = datetime.strptime(OverwriteImportDate, DateFormat).strftime(DateFormat)
                 
     payload = {
         'IsManual': IsManual,
